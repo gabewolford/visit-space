a5da8d9b2b67067b1eb5541d4ac85a0feac7a081
gabewolford <gabewolford@gmail.com>
2023-08-28 13:04:22 -0700

    nav styled

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,14 +1,64 @@
+'use client'
+
 import Link from "next/link"
+import Image from "next/image"
+import { usePathname } from 'next/navigation';
+import { Barlow_Condensed } from "next/font/google"
+
+const barlowCondensedRegular = Barlow_Condensed({ 
+    subsets: ['latin'],
+    weight: ["400"], 
+  })
+
+const barlowCondensedBold = Barlow_Condensed({ 
+    subsets: ['latin'],
+    weight: ["700"], 
+  })
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul className="flex flex-row space-x-6">
-                <Link href="/">Home</Link>
-                <Link href="/destination">Destination</Link>
-                <Link href="/crew">Crew</Link>
-                <Link href="/technology">Technology</Link>
-            </ul>
+        <nav className="flex flex-row justify-between pt-10">
+            <Link href="/" className="ml-12 flex items-center">
+                <Image 
+                    src="/assets/shared/logo.svg" 
+                    alt="nav logo" 
+                    width={48} 
+                    height={48} 
+                />
+            </Link>
+
+            <div className="flex flex-row bg-black-500 backdrop-blur-2xl h-[96px] w-[830px] justify-center">
+                <div className={`flex gap-12 ${barlowCondensedRegular.className} uppercase tracking-[2.7px]`}>
+                    <Link href="/">
+                        <div className="flex gap-2 border-b-[3px] border-transparent hover:border-b-[3px] hover:border-white-500/50 items-center h-full">
+                            <p className={barlowCondensedBold.className}>00</p>
+                            <p className={barlowCondensedRegular.className}>Home</p>
+                        </div>
+                    </Link>
+
+                    <Link href="/destination">
+                        <div className="flex gap-2 border-b-[3px] border-transparent hover:border-b-[3px] hover:border-white-500/50 items-center h-full">
+                            <p className={barlowCondensedBold.className}>01</p>
+                            <p className={barlowCondensedRegular.className}>Destination</p>
+                        </div>
+                    </Link>
+
+                    <Link href="/crew">
+                        <div className="flex gap-2 border-b-[3px] border-transparent hover:border-b-[3px] hover:border-white-500/50 items-center h-full">
+                            <p className={barlowCondensedBold.className}>02</p>
+                            <p className={barlowCondensedRegular.className}>Crew</p>
+                        </div>
+                    </Link>
+
+                    <Link href="/technology">
+                    <div className="flex gap-2 border-b-[3px] border-transparent hover:border-b-[3px] hover:border-white-500/50 items-center h-full">
+                            <p className={barlowCondensedBold.className}>03</p>
+                            <p className={barlowCondensedRegular.className}>Technology</p>
+                        </div>
+                    </Link>
+                    
+                </div>
+            </div>
         </nav>
     )
 }
