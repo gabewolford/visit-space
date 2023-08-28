1da0832d3388aad98300a29d863bf45e2b8c3ac0
gabewolford <gabewolford@gmail.com>
2023-08-28 16:12:43 -0700

    added titles to other pages, fixed letter spacing and font weight

--- src/app/destination/page.jsx
+++ src/app/destination/page.jsx
@@ -1,9 +1,9 @@
 import { Barlow_Condensed } from "next/font/google"
 import PlanetFacts from "../components/PlanetFacts"
 
-const barlowCondensedThin = Barlow_Condensed({ 
+const barlowCondensed = Barlow_Condensed({ 
   subsets: ['latin'],
-  weight: ['200'], 
+  weight: ['400'], 
 })
 
 const barlowCondensedBold = Barlow_Condensed({ 
@@ -16,7 +16,7 @@ export default function DestinationPage() {
       <main className="flex-1 md:flex-col lg:flex-row lg:mx-[165px] mx-10 items-center justify-around py-12">
         <div className="flex flex-row w-full gap-6 text-[28px]">
           <h4 className={`${barlowCondensedBold.className} tracking-[4.725px] text-white-500/25`}>01</h4>
-          <h4 className={`${barlowCondensedThin.className} uppercase`}>Pick your destination</h4>
+          <h4 className={`${barlowCondensed.className} tracking-[4.725px] uppercase`}>Pick your destination</h4>
         </div>
 
         <div className="flex flex-col my-16 justify-around">
--- src/app/technology/page.jsx
+++ src/app/technology/page.jsx
@@ -1,5 +1,29 @@
+import { Barlow_Condensed } from "next/font/google"
+
+const barlowCondensed = Barlow_Condensed({ 
+  subsets: ['latin'],
+  weight: ['400'], 
+})
+
+const barlowCondensedBold = Barlow_Condensed({ 
+  subsets: ['latin'],
+  weight: ['700'], 
+})
+
 export default function TechnologyPage() {
-  return (
-    <div>Technology Page</div>
-  )
-}
+    return (
+      <main className="flex-1 md:flex-col lg:flex-row lg:mx-[165px] mx-10 items-center justify-around py-12">
+        <div className="flex flex-row w-full gap-6 text-[28px]">
+          <h4 className={`${barlowCondensedBold.className} tracking-[4.725px] text-white-500/25`}>03</h4>
+          <h4 className={`${barlowCondensed.className} tracking-[4.725px] uppercase`}>Space Launch 101</h4>
+        </div>
+
+        <div className="flex flex-col my-16 justify-around">
+          <div className="flex flex-col lg:flex-row ">
+
+          </div>
+        </div>
+      </main>
+    )
+  }
+  
