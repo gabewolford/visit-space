0b173ffa66006e9f773f0de8fb800189b8ea9854
gabewolford <gabewolford@gmail.com>
2023-08-28 10:22:18 -0700

    added font colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blacknavy: {
+          500: '#0B0D17',
+        },
+        lightblue: {
+          500: '#D0D6F9',
+        },
+        white: {
+          500: '#FFFFFF',
+        },
       },
     },
   },
